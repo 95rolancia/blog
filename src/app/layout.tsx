8f64a7a6e794cddd823306a6e51e609823653fdd
jjangjun <adp0615@gmail.com>
2023-04-10 11:38:24 +0900

    fix: prettier 오류 수정

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,12 @@
-import { Noto_Sans } from "next/font/google"
-import "./globals.css"
+import { Noto_Sans } from 'next/font/google'
+import './globals.css'
 
 export const metadata = {
   title: "jjangjun's blog",
   description: "jjangjun's blog",
 }
 
-const notoSans = Noto_Sans({ subsets: ["latin"], weight: "500" })
+const notoSans = Noto_Sans({ subsets: ['latin'], weight: '500' })
 
 export default function RootLayout({
   children,
