e41286511edd0b2a903ab13e1ddacbe5fce69b90
jjangjun <95jjangjun@gmail.com>
2023-09-06 13:45:14 +0900

    feat: 게시글 상세 페이지

---
+++ src/app/posts/[slug]/page.tsx
@@ -0,0 +1,34 @@
+import { Metadata } from 'next'
+import { getAllPosts, getPostData } from '@/lib/posts'
+import PostContent from '@/components/PostContent'
+
+type Props = {
+  params: {
+    slug: string
+  }
+}
+
+export async function generateMetadata({ params: { slug } }: Props): Promise<Metadata> {
+  const { title } = await getPostData(slug)
+  return {
+    title,
+  }
+}
+
+export default async function PostPage({ params: { slug } }: Props) {
+  const post = await getPostData(slug)
+  const { title, path } = post
+
+  return (
+    <article className="rounded-2xl overflow-hidden bg-gray-100 shadow-lg m-4">
+      <PostContent post={post} />
+    </article>
+  )
+}
+
+export async function generateStaticParams() {
+  const posts = await getAllPosts()
+  return posts.map((post) => ({
+    slug: post.path,
+  }))
+}
