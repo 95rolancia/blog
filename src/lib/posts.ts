fe38eec615f0e99e7f88086825d67c6e20e8fdd3
jjangjun <95jjangjun@gmail.com>
2023-09-06 13:43:11 +0900

    feat: path로 게시글 상세 데이터를 가져오는 getPostData()

--- src/lib/posts.ts
+++ src/lib/posts.ts
@@ -1,6 +1,5 @@
 import path from 'path'
 import { readFile } from 'fs/promises'
-import { readFileSync } from 'fs'
 
 export type Post = {
   title: string
@@ -8,6 +7,10 @@ export type Post = {
   path: string
   tags: string[]
   date: Date
+}
+
+export type PostData = Post & {
+  content: string
 }
 
 const postsDir = path.join(process.cwd(), 'data', 'posts')
@@ -22,4 +25,16 @@ export async function getAllPosts() {
 
 export async function getRecentPosts(postCount: number) {
   return (await getAllPosts()).slice(0, postCount)
+}
+
+export async function getPostData(fileName: string): Promise<PostData> {
+  const filePath = path.join(process.cwd(), 'data', 'posts', `${fileName}.md`)
+  const posts = await getAllPosts()
+  const post = posts.find((post) => post.path === fileName)
+
+  if (!post) throw new Error(`${fileName}에 해당하는 포스트를 찾을 수 없음`)
+
+  const content = await readFile(filePath, 'utf-8')
+
+  return { ...post, content }
 }
