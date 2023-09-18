4b04fffca46a791bb3e7ac04622842926ef72d7d
jjangjun <95jjangjun@gmail.com>
2023-09-18 13:49:18 +0900

    feat: 카테고리 별 필터링 기능이 추가된 게시글 페이지 구현

--- src/app/posts/page.tsx
+++ src/app/posts/page.tsx
@@ -1,4 +1,6 @@
 import { Metadata } from 'next'
+import PostsCategoryFilter from '@/components/PostsCategoryFilter'
+import { getAllPosts, getCategories } from '@/lib/posts'
 import PostList from '@/components/PostList'
 
 export const metadata: Metadata = {
@@ -6,6 +8,8 @@ export const metadata: Metadata = {
   description: '작성된 게시글 들',
 }
 
-export default function Posts() {
-  return <PostList />
+export default async function Posts() {
+  const categories = await getCategories()
+  const posts = await getAllPosts()
+  return <PostsCategoryFilter posts={posts} categories={categories} />
 }
