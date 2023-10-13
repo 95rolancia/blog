e58cf193b76f4b8a851bfef368e335f6b17924b8
jjangjun <95jjangjun@gmail.com>
2023-10-13 18:50:52 +0900

    feat: RecentPosts 수정

--- src/components/RecentPosts.tsx
+++ src/components/RecentPosts.tsx
@@ -1,12 +1,28 @@
 import { getRecentPosts } from '@/lib/posts'
 import CarouselPosts from './CarouselPosts'
+import PostCard from './PostCard'
+import Link from 'next/link'
 
 export default async function RecentPosts() {
   const recentPosts = await getRecentPosts(10)
   return (
-    <section className="bg-sky-50 p-4">
-      <h2 className="text-2xl font-bold pb-4">최근 게시글</h2>
-      <CarouselPosts posts={recentPosts} />
+    <section className="p-4">
+      <ul>
+        {recentPosts.map((post) => (
+          <li key={post.title} className="mb-12">
+            <Link href={`/posts/${post.path}`} target="_blank">
+              <h2 className="font-bold text-3xl mb-2">{post.title}</h2>
+            </Link>
+            <div className="flex gap-2 mb-2">
+              <h4 className="text-gray-400">{post.date.toString()}</h4>
+              <h4>{post.readingTimeInMinutes} min read</h4>
+            </div>
+            <h3>{post.desc}</h3>
+          </li>
+        ))}
+      </ul>
+
+      {/* <CarouselPosts posts={recentPosts} /> */}
     </section>
   )
 }
